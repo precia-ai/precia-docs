f9891dc0c052d3581c3f7398f189bcf50a280717
SyahrulApr86 <apriansyah.syahrul@gmail.com>
2026-08-22 19:31:44 +0700

    docs: koreksi klaim alur MEDICCA dan hapus halaman operator ganda

--- content/en/integrasi-simrs/_meta.js
+++ content/en/integrasi-simrs/_meta.js
@@ -1,5 +1,4 @@
 export default {
   index: 'SIMRS Integration',
-  'alur-operator-medicca': 'MEDICCA Operator Workflow',
   medicca: 'MEDICCA to PRECIA Transaction Flow'
 }
--- content/id/integrasi-simrs/_meta.js
+++ content/id/integrasi-simrs/_meta.js
@@ -1,5 +1,4 @@
 export default {
   index: 'Integrasi SIMRS',
-  'alur-operator-medicca': 'Alur Operator MEDICCA',
   medicca: 'Alur Transaksi MEDICCA ke PRECIA'
 }
